9a78513d89b6fb029cc59324b42fd13774f1804f
Ben Nordstrom <bennord@gmail.com>
2016-03-11 21:55:22 +0900

    ResourceWarehouse: Info text and Editor gui
    Added info text for parts module mouse-over.
    Added Editor gui to disable Warehouse in the VAB.

--- USITools/USITools/Logistics/USI_ModuleResourceWarehouse.cs
+++ USITools/USITools/Logistics/USI_ModuleResourceWarehouse.cs
@@ -5,13 +5,13 @@ namespace KolonyTools
         [KSPField(isPersistant = true)] 
         public bool transferEnabled = true;
 
-        [KSPEvent(guiName = "Disable Warehouse", active = true, guiActive = true)]
+        [KSPEvent(guiName = "Disable Warehouse", active = true, guiActive = true, guiActiveEditor = true)]
         public void DisableTransfer()
         {
             ToggleTransfer(false);
         }
 
-        [KSPEvent(guiName = "Enable Warehouse", active = true, guiActive = false)]
+        [KSPEvent(guiName = "Enable Warehouse", active = false, guiActive = true, guiActiveEditor = true)]
         public void EnableTransfer()
         {
             ToggleTransfer(true);
@@ -20,16 +20,23 @@ namespace KolonyTools
         private void ToggleTransfer(bool state)
         {
             transferEnabled = state;
-            Events["DisableTransfer"].guiActive = state;
-            Events["EnableTransfer"].guiActive = !state; 
+            Events["DisableTransfer"].active = state;
+            Events["EnableTransfer"].active = !state; 
             MonoUtilities.RefreshContextWindows(part);
         }
 
         public override void OnStart(StartState state)
         {
-            Events["DisableTransfer"].guiActive = transferEnabled;
-            Events["EnableTransfer"].guiActive = !transferEnabled; 
-           
+            Events["DisableTransfer"].active = transferEnabled;
+            Events["EnableTransfer"].active = !transferEnabled; 
+        }
+
+        // Info about the module in the Editor part list
+        public override string GetInfo()
+        {
+            return ".\n" + // Extra space at the top because the module name is so long...
+                "Shares resources between nearby warehouses\n\n" +
+                "Range: " + USITools.Logistics.LogisticsSetup.Instance.Config.ScavangeRange + "m";
         }
     }
 }
