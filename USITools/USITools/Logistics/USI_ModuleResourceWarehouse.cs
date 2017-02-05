094991ed04050e36c1b45668260aab2fb1599c74
Bob Palmer <rbpalmer_nrt@hotmail.com>
2017-02-04 23:24:04 -0500

    Fixed warehouse toggles

--- USITools/USITools/Logistics/USI_ModuleResourceWarehouse.cs
+++ USITools/USITools/Logistics/USI_ModuleResourceWarehouse.cs
@@ -3,10 +3,10 @@ namespace USITools
     [KSPModule("USI Warehouse")]
     public class USI_ModuleResourceWarehouse : PartModule
     {
-        [KSPField(guiName = "Planetary Warehouse", guiActive = false, guiActiveEditor = true, isPersistant = true), UI_Toggle(disabledText = "Off", enabledText = "On")]
+        [KSPField(guiName = "Planetary Warehouse", guiActive = true, guiActiveEditor = true, isPersistant = true), UI_Toggle(disabledText = "Off", enabledText = "On")]
         public bool soiTransferEnabled = true;
 
-        [KSPField(guiName = "Local Warehouse", guiActive = false, guiActiveEditor = true, isPersistant = true), UI_Toggle(disabledText = "Off", enabledText = "On")]
+        [KSPField(guiName = "Local Warehouse", guiActive = true, guiActiveEditor = true, isPersistant = true), UI_Toggle(disabledText = "Off", enabledText = "On")]
         public bool localTransferEnabled = true;
 
         // Info about the module in the Editor part list
